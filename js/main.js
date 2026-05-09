4c9315d998332d343857e93f2e1c0d428dd1870a
Kaityo1221 <rakugo100@gmail.com>
2026-05-09 11:42:30 +0900

    main.js を更新

--- js/main.js
+++ js/main.js
@@ -56,7 +56,39 @@ function openTab(tabId, button) {
     behavior: "smooth"
   });
 }
+function checkPassword() {
+  const input = document.getElementById("passwordInput");
+  const error = document.getElementById("loginError");
+  const loginScreen = document.getElementById("loginScreen");
+  const splashScreen = document.getElementById("splashScreen");
 
+  if (input.value.trim() === "CA2026") {
+    error.textContent = "";
+    input.blur();
+
+    // ログイン画面をすぐ消す
+    loginScreen.remove();
+
+    // すぐアイコン演出開始
+    splashScreen.classList.add("show");
+
+// 🔊 ログイン音
+const loginSound = document.getElementById("loginSound");
+loginSound.currentTime = 0;
+loginSound.volume = 0.08;
+
+setTimeout(() => {
+  loginSound.play().catch(() => {});
+}, 80);
+
+setTimeout(function () {
+  splashScreen.remove();
+}, 1400);
+
+  } else {
+    error.textContent = "パスコードが違います";
+  }
+}
 document.addEventListener("DOMContentLoaded", function () {
 
   const passwordInput =
