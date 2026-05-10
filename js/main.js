482cfa11d44e8232a824f4574dc281b0d2db5965
Kaityo1221 <rakugo100@gmail.com>
2026-05-10 14:17:34 +0900

    main.js を更新

--- js/main.js
+++ js/main.js
@@ -86,34 +86,7 @@ function checkPassword() {
   }
 }
 
-function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));
-}
-function waitForRender() {
-  return new Promise(resolve => requestAnimationFrame(resolve));
-}
-window.showQuiz = function () {
-  document.getElementById("quizModal").style.display = "flex";
-}
 
-window.checkQuiz = function () {
-  const q1 = document.querySelector('input[name="q1"]:checked')?.value;
-  const q2 = document.querySelector('input[name="q2"]:checked')?.value;
-  const q3 = document.querySelector('input[name="q3"]:checked')?.value;
-
-  if (!q1 || !q2 || !q3) {
-    alert("すべて選択してください");
-    return;
-  }
-
-  if (q1 === "40" && q2 === "hard" && q3 === "25") {
-    localStorage.setItem("quizPassed", window.QUIZ_VERSION);
-    document.getElementById("quizModal").style.display = "none";
-    alert("✔ 利用準備OK！ツールを使えます");
-  } else {
-    alert("もう一度確認してください\nヒント：基本距離は40mです");
-  }
-}
 function openAdminLogin() {
   const modal = document.getElementById("adminLoginModal");
   const input = document.getElementById("adminPasswordInput");
