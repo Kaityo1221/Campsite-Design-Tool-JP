9fec6ec20a74be8330ea2749e7158b8bd80e8065
Kaityo1221 <rakugo100@gmail.com>
2026-05-10 14:28:36 +0900

    util.js を更新

--- js/util.js
+++ js/util.js
@@ -61,3 +61,17 @@ function setLoadingText(text) {
     loadingText.textContent = text;
   }
 }
+function getDistanceMeters(a, b) {
+  const R = 6378137;
+  const lat1 = a.lat * Math.PI / 180;
+  const lat2 = b.lat * Math.PI / 180;
+  const dLat = (b.lat - a.lat) * Math.PI / 180;
+  const dLng = (b.lng - a.lng) * Math.PI / 180;
+
+  const h =
+    Math.sin(dLat / 2) ** 2 +
+    Math.cos(lat1) * Math.cos(lat2) *
+    Math.sin(dLng / 2) ** 2;
+
+  return 2 * R * Math.asin(Math.sqrt(h));
+}
