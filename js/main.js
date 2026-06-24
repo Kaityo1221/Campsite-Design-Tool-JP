279d7319baa19fde5cc2f8da83389544f847b67d
Kaityo1221 <rakugo100@gmail.com>
2026-06-25 08:16:08 +0900

    main.js を更新

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-window.APP_VERSION = "v6.1.1";
-window.APP_UPDATED = "2026-06-19";
+window.APP_VERSION = "v6.2.0";
+window.APP_UPDATED = "2026-06-25";
 window.ENABLE_QUIZ = true;
 window.QUIZ_VERSION = "beta2";
 const ADMIN_CODE = "she1ep";
