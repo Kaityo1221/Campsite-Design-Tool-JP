ec3f36e03172c77e9132d5900d0efac8d4a02fa4
Kaityo1221 <rakugo100@gmail.com>
2026-09-18 07:00:46 +0900

    release: connect Android Bridge 0.3.4 XPI

--- js/bridge-setup-config.js
+++ js/bridge-setup-config.js
@@ -5,7 +5,7 @@
     appleShortcutUrl: 'https://www.icloud.com/shortcuts/3ddc90767d114bfba66de3a5d1a12d01',
     appleManualSetupUrl: './bridge-shortcut-install.html',
     appleRuntimeVersion: '1.0.0',
-    androidExtensionUrl: '',
+    androidExtensionUrl: './downloads/campsite-bridge-android-0.3.4.xpi',
     androidRuntimeVersion: '0.3.4',
     androidReleaseLabel: 'M3.4',
     androidXpiSha256: '13e57ace3468832c171df06bb92c983d66a22bc0f390c0ff15d0df3deff741b6',
