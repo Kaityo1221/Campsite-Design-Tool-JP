ed9e39bbe821037c55f743f8f384caaf680e417f
Kaityo1221 <rakugo100@gmail.com>
2026-09-18 00:59:43 +0900

    feat: add Bridge setup distribution config

---
+++ js/bridge-setup-config.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict';
+
+  // Campsite Bridge setup distribution endpoints.
+  // Public release day should only require filling the two blank URLs below.
+  // Do not place unreleased/private artifacts here.
+  window.CampsiteBridgeSetupConfig = Object.freeze({
+    appleShortcutUrl: '',
+    androidExtensionUrl: '',
+    firefoxAndroidUrl: 'https://play.google.com/store/apps/details?id=org.mozilla.firefox',
+    wayfarerUrl: 'https://wayfarer.nianticlabs.com/new/'
+  });
+})();
