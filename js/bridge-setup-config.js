7ec8ec0bf1e7ac84657f2fcecbf983de4fc227a0
Kaityo1221 <rakugo100@gmail.com>
2026-09-18 02:15:49 +0900

    feat: configure Bridge setup distribution endpoints

---
+++ js/bridge-setup-config.js
@@ -0,0 +1,12 @@
+(() => {
+  'use strict';
+
+  window.CampsiteBridgeSetupConfig = Object.freeze({
+    appleShortcutUrl: '',
+    appleManualSetupUrl: './bridge-shortcut-install.html',
+    appleRuntimeVersion: '1.0.0',
+    androidExtensionUrl: '',
+    firefoxAndroidUrl: 'https://play.google.com/store/apps/details?id=org.mozilla.firefox',
+    wayfarerUrl: 'https://wayfarer.nianticlabs.com/new/'
+  });
+})();
