8e2eadec121cb041117809daac9dd32928fda542
Kaityo1221 <rakugo100@gmail.com>
2026-09-21 21:27:13 +0900

    bridge-setup: add PC Bridge release config

--- js/bridge-setup-config.js
+++ js/bridge-setup-config.js
@@ -10,6 +10,10 @@
     androidReleaseLabel: 'M3.4',
     androidXpiSha256: '13e57ace3468832c171df06bb92c983d66a22bc0f390c0ff15d0df3deff741b6',
     firefoxAndroidUrl: 'https://play.google.com/store/apps/details?id=org.mozilla.firefox',
+    pcExtensionUrl: './downloads/campsite-bridge-pc-0.1.0.zip',
+    pcRuntimeVersion: '0.1.0',
+    chromeDesktopUrl: 'https://www.google.com/chrome/',
+    pcWayfarerUrl: 'https://wayfarer.scopely.com/new/mapview',
     wayfarerUrl: 'https://wayfarer.nianticlabs.com/new/'
   });
 })();
